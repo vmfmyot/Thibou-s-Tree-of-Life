01ea189ba4e69f65d29376532407270f2f70204f
victoria.myot <victoria.myot@universite-paris-saclay.fr>
2025-03-28 17:26:49 +0100

    Class Node with all its attributes

---
+++ Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree_of_Life
+{
+    internal class Node
+    {
+        /****************
+        *  ATTRIBUTES  *
+        ****************/
+
+        int id;
+        string name;
+        int nbChildren;
+        bool isLeaf;
+        bool hasWebsite;
+
+        //Confidence in the placement of the node (in the tree)
+        enum Confidence { CONFIDENT, NOTCONFIDENT, UNKNOWN };
+
+        //Phylesis of the node
+        enum Phylesis { MONOPHYLETIC, UNCERTAIN, NOTMONOPHYLETIC };
+
+        Confidence confidence;
+        Phylesis phylesis;
+
+        Node parentNode;
+        ArrayList children; //list of children NODES (not ids)
+
+
+        /****************
+        *    METHODS    *
+        ****************/
+    }
+}
--- TreeofLife.cs
+++ TreeofLife.cs
@@ -6,5 +6,10 @@ namespace Tree_of_Life
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
