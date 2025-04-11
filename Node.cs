42ac76d63effa9adb58b27dbba8916326387f596
Sidonie Minodier <sidonie.minodier@universite-paris-saclay.fr>
2025-04-11 15:07:24 +0200

    test

--- Node.cs
+++ Node.cs
@@ -18,6 +18,8 @@ namespace Tree_of_Life
         int nbChildren;
         bool isLeaf;
         bool hasWebsite;
+        bool extinct;
+        bool isCluster;
 
         //Confidence in the placement of the node (in the tree)
         enum Confidence { CONFIDENT, NOTCONFIDENT, UNKNOWN };
@@ -25,15 +27,67 @@ namespace Tree_of_Life
         //Phylesis of the node
         enum Phylesis { MONOPHYLETIC, UNCERTAIN, NOTMONOPHYLETIC };
 
-        Confidence confidence;
+        Confidence  confidence;
         Phylesis phylesis;
 
-        Node parentNode;
-        ArrayList children; //list of children NODES (not ids)
+        Node? parentNode;
+        ArrayList? children; //list of children NODES (not ids)
 
 
         /****************
         *    METHODS    *
         ****************/
+
+        public Node(String[] list){
+            id = Int32.Parse(list[0]);
+            name= list[1];
+            nbChildren= Int32.Parse(list[2]);
+            isLeaf= stringToBool(list[3]);
+            hasWebsite= stringToBool(list[4]);
+            extinct= stringToBool(list[5]);
+            confidence = stringToConfidence(list[6]);
+            phylesis = stringToPhylesis(list[7]);
+
+            children=new ArrayList();
+
+            isCluster= nbChildren > 5;
+        }
+
+        public int getId() { return id; }
+
+        public void setParentNode(Node parent) { parentNode = parent; }
+        public void addChild(Node child) { children.Add(child); }
+
+        /** TODO
+         */
+        private Confidence stringToConfidence(string i)
+        {
+            switch (i)
+            {
+                case "0": return Confidence.UNKNOWN;
+                case "1": return Confidence.CONFIDENT;
+                default: return Confidence.NOTCONFIDENT;
+            }
+        }
+
+        private Phylesis stringToPhylesis(string i)
+        {
+            switch (i)
+            {
+                case "0": return Phylesis.MONOPHYLETIC;
+                case "1": return Phylesis.UNCERTAIN;
+                default: return Phylesis.UNCERTAIN;
+            }
+        }
+
+        private Boolean stringToBool(string i)
+        {
+            switch (i)
+            {
+                case "1": return true;
+                default: return false;
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using System.Diagnostics;
+using System.Collections;
+
+
 namespace Tree_of_Life
 {
     internal static class Program
@@ -6,10 +10,59 @@ namespace Tree_of_Life
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
+
+        public static Node? getNode(ArrayList nodeList, int nodeId)
+        {
+            foreach (Node node in nodeList)
+            {
+                if (node.getId() == nodeId) 
+                {
+                    return node; 
+                } 
+            }
+            return null;
+        }
+
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+            var links = new StreamReader(File.OpenRead("../../../data/treeoflife_links.csv"));
+            var nodes = new StreamReader(File.OpenRead("../../../data/treeoflife_nodes.csv"));
+
+            Debug.Print(nodes.ReadLine()); //on enleve l'entete
+
+            //Création des nodes
+            ArrayList nodesList = new ArrayList();
+            while (!nodes.EndOfStream){
+                string line = nodes.ReadLine();
+                Debug.Print(line);
+                string[] values = line.Split(',');
+
+                if (values.Length == 8)
+                {
+                    nodesList.Add(new Node(values));
+                }
+            }
+
+            //création des liens
+            links.ReadLine();
+            while (!links.EndOfStream)
+            {
+                string line = links.ReadLine();
+                Debug.Print(line);
+                string[] values = line.Split(',');
+
+                Node source=getNode(nodesList, line[0]);
+                Node target= getNode(nodesList, line[1]);
+                if (source != null && target != null)
+                {
+                    source.setParentNode(target);
+                    target.addChild(source);
+                }
+            }
+
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
